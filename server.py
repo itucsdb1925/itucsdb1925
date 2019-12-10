25235508d6f8e7128136a13be62436a8f9a4e7bb
Omerguler <kerpetenci@gmail.com>
2019-12-10 22:31:25 +0300

    Added bootstrap styling.

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def load_user(user_id):
 def create_app():
   app = Flask(__name__)
   app.config.from_object("settings")
-  app.add_url_rule("/signin", view_func=views.sign_in,methods=['GET','POST'])
+  app.add_url_rule("/", view_func=views.sign_in,methods=['GET','POST'])
   app.add_url_rule("/signup", view_func=views.sign_up,methods=['GET','POST'])
   app.add_url_rule("/update", view_func=views.update,methods=['GET','POST'])
   app.add_url_rule("/signedin", view_func=views.home_page,methods=['GET','POST'])
