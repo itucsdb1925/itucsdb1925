ef5382dac9b76a0c1531c1358bc478987fe4598a
İbrahim Ethem Türkeri <iethem3842@gmail.com>
2019-12-16 23:14:34 +0300

    fix 5888

--- server.py
+++ server.py
@@ -9,21 +9,18 @@ lm = LoginManager()
 def load_user(user_id):
     return get_user(user_id)
 
-def create_app():
-  app = Flask(__name__)
-  app.config.from_object("settings")
-  app.add_url_rule("/", view_func=views.sign_in,methods=['GET','POST'])
-  app.add_url_rule("/signup", view_func=views.sign_up,methods=['GET','POST'])
-  app.add_url_rule("/update", view_func=views.update,methods=['GET','POST'])
-  app.add_url_rule("/signedin", view_func=views.home_page,methods=['GET','POST'])
-  app.add_url_rule("/signout", view_func=views.sign_out, methods=['GET','POST'])
-  app.add_url_rule("/employees",view_func=views.employees_page, methods=['GET','POST'])
-  app.add_url_rule("/transactions",view_func=views.transaction_page, methods=['GET'])
-  app.add_url_rule("/request",view_func=views.request_page, methods=['GET','POST'])
-  lm.init_app(app)
-  lm.login_view = "sign_in"            
-  return app
+app = Flask(__name__)
+app.config.from_object("settings")
+app.add_url_rule("/", view_func=views.sign_in,methods=['GET','POST'])
+app.add_url_rule("/signup", view_func=views.sign_up,methods=['GET','POST'])
+app.add_url_rule("/update", view_func=views.update,methods=['GET','POST'])
+app.add_url_rule("/signedin", view_func=views.home_page,methods=['GET','POST'])
+app.add_url_rule("/signout", view_func=views.sign_out, methods=['GET','POST'])
+app.add_url_rule("/employees",view_func=views.employees_page, methods=['GET','POST'])
+app.add_url_rule("/transactions",view_func=views.transaction_page, methods=['GET'])
+app.add_url_rule("/request",view_func=views.request_page, methods=['GET','POST'])
+lm.init_app(app)
+lm.login_view = "sign_in"            
 
 if __name__ == "__main__":
-  my_app = create_app()
-  my_app.run()
+  app.run()
